e9f5aba43ccf69dc83916fd01c80dd8a0f0fccc1
Poorvi <poorvip@argusoft.com>
2023-10-04 01:15:57 +0530

    some javascript array practice

---
+++ index.js
@@ -0,0 +1,21 @@
+// Write a function that takes an array (a) and a value (n) as argument. Return the nth element of 'a'
+function myFunction(a, n){
+ return a[n-1];
+}
+
+
+// Write a function that takes an array (a) as argument. Remove the first 3 elements of 'a'. Return the result
+function myFunction(a){
+   return a.slice(3);
+}
+
+
+// Write a function that takes an array (a) as argument. Extract the last 3 elements of 'a'. Return the resulting array
+function myFunction(a){
+   return a.slice(-3);
+}
+
+// Write a function that takes an array (a) as argument. Extract the first 3 elements of a. Return the resulting array
+function myFunction(a){
+    return 
+}
