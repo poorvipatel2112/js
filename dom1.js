126a240355fc23d7e660262c2451233e0ab46d52
Poorvi <poorvip@argusoft.com>
2023-10-05 09:18:30 +0530

    few changes are made

--- dom1.js
+++ dom1.js
@@ -25,3 +25,35 @@ for(let color of colors){
 //         alert(color)
 //     });
 // }
+
+
+const input = document.querySelector('#username');
+input.addEventListener('keydown',function(e){
+    console.log('KEY DOWN!');
+});
+
+
+input.addEventListener('keyup',function(e){
+    console.log('KEY UP!');
+});
+
+input.addEventListener('keypress',function(e){
+    console.log('KEY PRESS!');
+});
+
+const addItem = document.querySelector('#addItem');
+const items = document.querySelector('#items');
+
+addItem.addEventListener('keypress',function(e){
+    if(e.key === 'Enter'){
+        if(!this.value) return
+        //add a new item to list
+        const newItemText = this.value;
+        const newItem = document.createElement('li');
+        newItem.innerText = newItemText;
+        items.appendChild(newItem);
+        this.value = '';
+
+    }
+})
+
