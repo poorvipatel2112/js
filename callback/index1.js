25c16cc6adfc2df1b2784158eb251702ffbb5bc9
Poorvi <poorvip@argusoft.com>
2023-10-06 21:22:00 +0530

    callback

--- callback/index1.js
+++ callback/index1.js
@@ -8,13 +8,21 @@ setTimeout(() =>{
 },1000);
 
 
-//better approach
+// better approach
 const moveX = (element,amount,delay, callback) =>{
-    setTimeout (() =>{
-        element.style.transform = `translateX(${amount}px)`;
-        if(callback) callback();
-    },delay);
-}
+    const bodyBoundary =document.body.clientWidth;
+    const elRight = element.getBoundingClientRect().right;
+    const currLeft = element.getBoundingClientRect().left;
+    if(elRight + amount > bodyBoundary){
+        console.log('Done- cannot go that far');
+    }
+    else{
+        setTimeout (() =>{
+            element.style.transform = `translateX(${amount}px)`;
+            if(callback) callback();
+        },delay);
+    }
+};
 
 moveX(btn,100,1000, () =>{
     moveX(btn,200,2000,() =>{
@@ -23,3 +31,20 @@ moveX(btn,100,1000, () =>{
         });
     });
 });
+
+
+
+// const moveX = (element, amount,delay,successCallback,failCallback) =>{
+//     setTimeout(() =>{
+//         const max = document.body.clientWidth;
+//         const dimensions = element.getBoundingClientRect();
+//         const currentRight = dimensions.x + dimensions.width;
+//         if(currentRight + amount>max){
+//             failCallback();
+//         }
+//         else{
+//             element.style.transform = `translateX(${currentRight + amount}px)`;
+//             successCallback();
+//         }
+//     },delay);
+// };
