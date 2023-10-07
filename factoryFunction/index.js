61286c40c933325d7949d0b098047953f9b637fc
Poorvi <poorvip@argusoft.com>
2023-10-07 10:25:39 +0530

    practiced factory function using Object.create method

--- factoryFunction/index.js
+++ factoryFunction/index.js
@@ -26,3 +26,25 @@ let personTwo = createPerson('Nishit' , 'Patel')
 
 console.log(personOne.getFullName());
 console.log(personTwo.getFullName());
+
+//using Object.create() method to create object as it is the most efficient method for factory function to create object.
+
+let personActions = {
+    getFullName(){
+        return this.firstname + ' ' + this.lastname;
+    },
+}
+
+function createPersons(firstname,lastname){
+    //let objectname = Object.create(proto)
+    let person = Object.create(personActions);
+    person.firstname = firstname;
+    person.lastname = lastname;
+    return person;
+}
+
+let personA = createPersons('Laxmi', 'Patel');
+let personB = createPersons('Keshav' , 'Patel');
+
+console.log(personA.getFullName());
+console.log(personB.getFullName());
