a461a4873227d7a68d1b93bb6b064917edb64a1e
Poorvi <poorvip@argusoft.com>
2023-10-07 10:16:40 +0530

    factory function used to create object

--- factoryFunction/index.js
+++ factoryFunction/index.js
@@ -1,3 +1,4 @@
+//maunally creating a object
 let person1 = {
     firstname: 'Poorvi',
     lastname: 'Patel',
@@ -5,5 +6,23 @@ let person1 = {
         return this.firstname + this.lastname;
     },
 };
-
 console.log(person1.getFullName());
+
+
+
+//creating object using factory function
+function createPerson(firstname , lastname){
+    return {
+        firstname : firstname,
+        lastname : lastname,
+        getFullName(){
+            return firstname + " " +  lastname;
+        },
+    };
+}
+
+let personOne = createPerson('Rachit' , 'Patel');
+let personTwo = createPerson('Nishit' , 'Patel')
+
+console.log(personOne.getFullName());
+console.log(personTwo.getFullName());
