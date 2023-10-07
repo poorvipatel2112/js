da73b99e9e813c0fe6ab7abd4fb998aea2db81f8
Poorvi <poorvip@argusoft.com>
2023-10-07 12:33:55 +0530

    pratice on constructor function

--- constructor function/index.js
+++ constructor function/index.js
@@ -11,3 +11,28 @@ function Person(firstName , lastName){
 let person = new Person('Poorvi' , 'Patel');
 console.log(person.getFullName());
 
+//another practice code on constructor function
+
+function Employee(firstname , lastname , salary){
+    this.firstname = firstname;
+    this.lastName = lastname;
+    this.salary = salary;
+
+    this.getName = function(){
+        return firstname + " " + lastname;
+    }
+
+    this.details = function(){
+        return "The salary of employee " + this.getName() + " is " + salary;
+    }
+}
+
+let emp1 = new Employee('Poorvi', 'Patel' , 20000);
+let emp2 = new Employee('Kanchan', 'Sikder' , 20000);
+
+console.log(emp1.getName());
+console.log(emp2.getName());
+
+console.log(emp1.details());
+console.log(emp2.details());
+
