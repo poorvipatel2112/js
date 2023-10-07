a1b5002a483be40aefc309d236d621ff287101e0
Poorvi <poorvip@argusoft.com>
2023-10-07 17:59:39 +0530

    sequential request async

--- await and async/index.js
+++ await and async/index.js
@@ -1,5 +1,15 @@
-//parallel request
+//sequential request
+async function get3PokemonSequential() {
+	const poke1 = await axios.get('https://pokeapi.co/api/v2/pokemon/1');
+	const poke2 = await axios.get('https://pokeapi.co/api/v2/pokemon/2');
+	const poke3 = await axios.get('https://pokeapi.co/api/v2/pokemon/3');
+	console.log(poke1.data);
+	console.log(poke2.data);
+	console.log(poke3.data);
+}
 
+
+//parallel request
 async function get3Pokemon(){
     const prom1 = axios.get('https://pokeapi.co/api/v2/pokemon/1');
     const prom2 = axios.get('https://pokeapi.co/api/v2/pokemon/2');
